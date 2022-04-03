ec07bc323ef088f8d13e46c50febb63f46daca0e
guzzler13 <stephanedasil@gmail.com>
2022-04-03 11:52:15 +0200

    first route with next => easyyy !

---
+++ pages/contact/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function index() {
+  return (
+    <>
+      <h1>This is page CONTACT</h1>
+      <style jsx>{`
+        h1 {
+          color: red;
+          text-align: center;
+        }
+      `}</style>
+    </>
+  )
+}
